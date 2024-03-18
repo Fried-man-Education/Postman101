e3b3d847232c8cd28ccf5e89f5668f3174caac94
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-17 22:31:43 -0400

    Add description

--- Introduction to API testing & JavaScript/Exercise_01.js
+++ Introduction to API testing & JavaScript/Exercise_01.js
@@ -1,4 +1,10 @@
 // Working with JavaScript variables
+/*
+The following code has already defined two JavaScript variables.
+Make the following changes below line #3:
+Create a 3rd JavaScript variable named city and assign it the value: New York.
+Set the value of the variable email to jamie.r@example.com
+*/
 let firstName = "Jamie";
 let email;
 // WRITE YOUR CODE BELOW THIS LINE
