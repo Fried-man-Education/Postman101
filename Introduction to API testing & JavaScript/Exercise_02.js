91ac5ccd32b5ae550e19f7145ce1b0089484bbac
Andrew Friedman <andrewfriedman01@gmail.com>
2024-03-17 22:33:08 -0400

    Add solution to exercise 2 - intro

---
+++ Introduction to API testing & JavaScript/Exercise_02.js
@@ -0,0 +1,6 @@
+// Swap two variables in JavaScript
+/*
+The values for variables firstName and lastName need to be switched. The firstName needs to have the value Jamie, while the variable lastName needs to have the value Doe.
+Without altering the initial variable definition, switch the values between the two variables.
+Hint: Use a temporary variable named temp to hold the value of one of the variables.
+*/
